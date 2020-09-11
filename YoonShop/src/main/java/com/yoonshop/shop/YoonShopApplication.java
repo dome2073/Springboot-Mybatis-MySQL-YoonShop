7e13c524dc6dc3c55b103e8b5c435f7f9723a6ff
dome2073 <wlgns2073@gmail.com>
2020-09-11 15:19:35 +0900

    기초 템플릿 적용 및 git 테스트

---
+++ YoonShop/src/main/java/com/yoonshop/shop/YoonShopApplication.java
@@ -0,0 +1,13 @@
+package com.yoonshop.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoonShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoonShopApplication.class, args);
+	}
+
+}
---
+++ YoonShop/src/test/java/com/yoonshop/shop/YoonShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoonshop.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoonShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
