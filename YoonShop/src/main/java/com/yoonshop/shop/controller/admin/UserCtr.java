eaae514854246be9fc5da3617ec16b642853f583
dome2073 <wlgns2073@gmail.com>
2020-09-13 23:16:01 +0900

    모델 추가, 추가 서비스 등록

---
+++ YoonShop/src/main/java/com/yoonshop/shop/controller/admin/UserCtr.java
@@ -0,0 +1,66 @@
+package com.yoonshop.shop.controller.admin;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.yoonshop.shop.model.User;
+import com.yoonshop.shop.service.admin.UserSvc;
+import com.google.gson.Gson;
+
+@Controller
+@RequestMapping("/admin/user")
+public class UserCtr {
+	
+	@Autowired
+	private UserSvc userSvc;
+	
+	@GetMapping("")
+	public String userPathRedirect() {
+		return "redirect:/admin/user/";
+	}
+	
+	@GetMapping("/")
+	public String user(Model model) {
+		return "/admin/user";
+	}
+	
+	@GetMapping("/check")
+	public @ResponseBody Map<String, Object> userCheck() {
+		int result = userSvc.userCheck();
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("data", result);
+		
+		return map;
+	}
+	
+	@GetMapping("/list")
+	public @ResponseBody String userList() {
+		List<User> user = userSvc.userList();
+		
+		Gson gson = new Gson();
+		String userToJson = gson.toJson(user);
+		
+		userToJson = "{\"data\":"+userToJson+"}";
+		return userToJson;
+	}
+	
+	@PostMapping("/delete")
+	public @ResponseBody Map<String, Object> userDelete(){
+		String result = userSvc.userDelete();
+
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("data", result);
+		
+		return map;
+	}
+}
---
+++ YoonShop/src/main/java/com/yoonshop/shop/model/Sale.java
@@ -0,0 +1,26 @@
+package com.yoonshop.shop.model;
+
+import java.sql.Timestamp;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Sale {
+	private int id;
+	private int productId;
+	private int amount;
+	private String unitCd;
+	private String startDate;
+	private String endDate;
+	private int insId;
+	private Timestamp insDt;
+	private int updId;
+	private Timestamp updDt;
+	
+	private String productNm;
+	private String categoryCd;
+	private int price;
+	private String unitNm; 
+}
