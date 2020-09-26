2249744095f535dd3b06052645b5a40e05717932
dome2073 <wlgns2073@gmail.com>
2020-09-26 21:42:23 +0900

    repository 추가

---
+++ YoonShop/src/main/java/com/yoonshop/shop/repository/CouponRepository.java
@@ -0,0 +1,15 @@
+package com.yoonshop.shop.repository;
+
+import java.util.List;
+
+import com.example.shop.model.Coupon;
+
+public interface CouponRepository{
+	//ADMIN
+	List<Coupon> findAll();
+	int save(Coupon coupon);
+	int update(Coupon coupon);
+	
+	//USER
+	Coupon findCoupon(String code);
+}
